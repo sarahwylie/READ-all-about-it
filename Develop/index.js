bcf1be04ed58a321835f877325eb173bdd798c6c
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-08 12:10:02 -0600

    just starting questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// const {writeFile, copyFile} = require('./utils/generateMarkdown.js');
+// const generatePage = require("./src/page-template.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project (be sure to explain the what/why/how - why does this project exist?). (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the required steps to install this project? (Required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please let us know how to install your project!');
+                    return false;
+                }
+            }
+        },
+    ]);
+}
+
+questions();
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
